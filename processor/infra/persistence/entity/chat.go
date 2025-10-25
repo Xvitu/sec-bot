c113d9177edb1ba5c59bfc21ab85a5f4967f668b
Xvitu <vas17.vs@gmail.com>
2025-10-25 19:21:36 +0000

    chore: add origin to chat

--- processor/infra/persistence/entity/chat.go
+++ processor/infra/persistence/entity/chat.go
@@ -4,13 +4,14 @@ import "xvitu/sec-bot/domain"
 import domainEntity "xvitu/sec-bot/domain/entity"
 
 type Chat struct {
-	Id            int         `dynamodbav:"id"`
-	ExternalId    string      `dynamodbav:"external_id"`
-	UserId        string      `dynamodbav:"user_id"`
-	LastMessageID string      `dynamodbav:"last_message_id"`
-	CreatedAt     string      `dynamodbav:"created_at"`
-	UpdatedAt     string      `dynamodbav:"updated_at"`
-	Step          domain.Step `dynamodbav:"step"`
+	Id            string        `dynamodbav:"id"`
+	ExternalId    string        `dynamodbav:"external_id"`
+	UserId        string        `dynamodbav:"user_id"`
+	LastMessageID string        `dynamodbav:"last_message_id"`
+	CreatedAt     string        `dynamodbav:"created_at"`
+	UpdatedAt     string        `dynamodbav:"updated_at"`
+	Step          domain.Step   `dynamodbav:"step"`
+	Origin        domain.Origin `dynamodbav:"origin"`
 }
 
 func FromDomain(chat domainEntity.Chat) Chat {

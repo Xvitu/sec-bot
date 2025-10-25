35cec60cf8a607e85bb8bac0d32540e1d3ce3d71
Xvitu <vas17.vs@gmail.com>
2025-10-25 03:49:09 +0000

    chore: create dynamo client

---
+++ processor/infra/persistence/dynamo/client.go
@@ -0,0 +1,33 @@
+package dynamodbclient
+
+import (
+	"context"
+	"fmt"
+	"xvitu/sec-bot/shared/env"
+
+	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/config"
+	"github.com/aws/aws-sdk-go-v2/service/dynamodb"
+)
+
+func NewClient(ctx context.Context) (*dynamodb.Client, error) {
+
+	envs := env.Get()
+
+	endpoint := envs.DynamoDBEndpoint
+	region := envs.AwsRegion
+	if region == "" {
+		region = "us-east-1"
+	}
+
+	cfg, err := config.LoadDefaultConfig(ctx, config.WithRegion(region))
+	if err != nil {
+		return nil, fmt.Errorf("falha ao carregar configuração AWS: %w", err)
+	}
+
+	if envs.Env == "local" {
+		cfg.BaseEndpoint = aws.String(endpoint)
+	}
+
+	return dynamodb.NewFromConfig(cfg), nil
+}

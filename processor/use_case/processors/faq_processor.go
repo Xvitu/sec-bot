1f8d6799412634a22fd22897e096a6dee297d0de
Xvitu <vas17.vs@gmail.com>
2025-10-31 19:03:29 +0000

    chore: create faq processor

---
+++ processor/use_case/processors/faq_processor.go
@@ -0,0 +1,52 @@
+package processors
+
+import (
+	"context"
+	"fmt"
+	"xvitu/sec-bot/domain"
+	domainEntity "xvitu/sec-bot/domain/entity"
+	"xvitu/sec-bot/entypoint/dto"
+	"xvitu/sec-bot/infra/persistence/repository"
+	"xvitu/sec-bot/infra/telegram"
+)
+
+type FaqProcessor struct {
+	chatRepository    *repository.ChatRepository
+	telegramGateway   *telegram.Gateway
+	messageRepository *repository.MessageRepository
+}
+
+const backMessageId = "11"
+
+func (p *FaqProcessor) Execute(chatUpdate dto.Chat, chat *domainEntity.Chat) (*domainEntity.Chat, error) {
+	if chatUpdate.Message == backMessageId {
+		return p.handleMessage(domain.Start, "greetings", chat)
+	}
+
+	chat, chatError := p.handleMessage(domain.Faq, "faq_"+chatUpdate.Message, chat)
+	if chatError != nil {
+		return p.handleMessage(domain.Faq, "invalid_option", chat)
+	}
+
+	return chat, nil
+}
+
+func (p *FaqProcessor) handleMessage(step domain.Step, messageId string, chat *domainEntity.Chat) (*domainEntity.Chat, error) {
+	replyMessage := p.messageRepository.GetByStepAndMessageId(step, messageId)
+	if replyMessage == nil {
+		return nil, fmt.Errorf("no Message Found")
+	}
+
+	_, sendMessageError := p.telegramGateway.SendMessage(chat.ExternalId, replyMessage.Text)
+	if sendMessageError != nil {
+		return nil, fmt.Errorf("error while sending message: %s", sendMessageError)
+	}
+
+	chat.Step = step
+	chat.LastMessageID = messageId
+	saveError := p.chatRepository.Save(context.Background(), *chat)
+	if saveError != nil {
+		return nil, fmt.Errorf("error while saving chat: %s", saveError)
+	}
+	return chat, nil
+}
